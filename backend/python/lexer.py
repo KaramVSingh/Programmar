849ff96789b3ed5f752aab04c271d2a694a2b06d
Karam Singh <karamsingh175@gmail.com>
2019-07-29 03:22:31 -0400

    Added regex rules to README

---
+++ backend/python/lexer.py
@@ -0,0 +1,7 @@
+# This file contains a lexer which is able to take a context free grammar and produce a linked list of tokens
+# The input is a JSON formatted CFG (See README) and the output is a linked list of tokens
+
+import json
+
+def lex(cfg: json):
+    pass
